0378e2494cb505d77e0356ae4deb3e10bdfd0f9c
camillyMelo <melocamilly@gmail.com>
2023-04-02 12:04:13 -0300

    Commit inicial - telinhas

---
+++ app/src/main/java/com/example/listacontato/view/ActivityContact.java
@@ -0,0 +1,16 @@
+package com.example.listacontato.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.listacontato.R;
+
+public class ActivityContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
---
+++ app/src/main/java/com/example/listacontato/view/ActivityNewContact.java
@@ -0,0 +1,16 @@
+package com.example.listacontato.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.listacontato.R;
+
+public class ActivityNewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_contact);
+    }
+}
