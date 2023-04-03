1e73419b67004fc1ef262c7ef39ee3497c75e8eb
camillyMelo <93149857+camillyMelo@users.noreply.github.com>
2023-04-03 10:46:21 -0300

    Update ContactSpinnerAdapter.java

--- app/src/main/java/com/example/listacontato/view/adapter/ContactSpinnerAdapter.java
+++ app/src/main/java/com/example/listacontato/view/adapter/ContactSpinnerAdapter.java
@@ -15,4 +15,28 @@ public class ContactSpinnerAdapter extends ArrayAdapter<Contact> {
     public ContactSpinnerAdapter(@NonNull Context context, int resource, @NonNull List<Contact> values) {
         super(context, resource, values);
     }
+    
+     @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        TextView textView = new TextView(getContext());
+        if(getItem(position) == null){
+            textView.setText("");
+        }else {
+            textView.setText(getItem(position).getApelido());
+        }
+        return textView;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        TextView textView = new TextView(getContext());
+        if(getItem(position) == null){
+            textView.setText("");
+        }else {
+            textView.setText(String.format("Apelido: %s/nNome: %s", getItem(position).getApelido(), getItem(position).getName()));
+        }
+        textView.setPadding(8, 8, 8, 8);
+        return textView;
+    }
 }
